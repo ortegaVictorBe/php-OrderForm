612e97af2c666cedcc5e51865f6419bded384229
Ortega <ortega.victor@gmail.com>
2020-09-10 18:15:38 +0200

    Add form validations, error message, delivery options

--- index.php
+++ index.php
@@ -3,7 +3,124 @@
 declare(strict_types=1);
 
 //we are going to use session variables so we need to enable sessions
-session_start();
+SESSION_START();
+//Variables
+$addressStreet=(!empty($_SESSION["s_addressStreet"])) ? $_SESSION["s_addressStreet"] : "";
+$streetNumber=(!empty($_SESSION["s_streetNumber"])) ? $_SESSION["s_streetNumber"] : "";
+$city=(!empty($_SESSION["s_city"])) ? $_SESSION["s_city"]:"";
+$zipCode=(!empty($_SESSION["s_zipCode"])) ? $_SESSION["s_zipCode"] : "";
+
+
+$email= "";
+$selectedProducts=[];
+$errMessage="";
+$totalValue = 0;
+
+ 
+//gettin the options to order food=1 : order food,  food=0: order drinks
+$food=(isset($_GET["food"])) ? $_GET["food"] : "1";
+
+//your products with their price.
+if ($food == '1'){
+$products = [
+  ['name' => 'Club Ham', 'price' => 3.20],
+  ['name' => 'Club Cheese', 'price' => 3],
+  ['name' => 'Club Cheese & Ham', 'price' => 4],
+  ['name' => 'Club Chicken', 'price' => 4],
+  ['name' => 'Club Salmon', 'price' => 5] 
+];
+}else{  
+  $products = [
+    ['name' => 'Cola', 'price' => 2],
+    ['name' => 'Fanta', 'price' => 2],
+    ['name' => 'Sprite', 'price' => 2],
+    ['name' => 'Ice-tea', 'price' => 3],
+];
+
+}
+
+function cleanInput($data){
+    $data=trim($data);
+    $data=stripslashes($data);
+    $data=htmlspecialchars($data);
+    return $data;
+};
+
+function validateData():bool{
+    
+    global $errMessage,$email,$addressStreet,$streetNumber,$city,$zipCode,$selectedProducts,$products; // TODO: fix dirty code    
+
+    //Validate e-mail 
+    if (empty($_POST["email"])){
+        $errMessage=$errMessage."e-mail is empty !";
+    }else{
+     $email=cleanInput($_POST["email"]);
+     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errMessage=$errMessage."Invalidate email !";
+     } 
+    }
+ 
+    //validate address-street
+    if (empty($_POST["street"])){
+         $errMessage=$errMessage.", Street name is empty !";
+    }else{
+     $addressStreet=cleanInput($_POST["street"]); 
+     if (!preg_match("/^[a-zA-Z-' ]*$/",$addressStreet)) {
+        $errMessage=$errMessage.", Street name Only letters and white space allowed !";        
+      }
+    }
+ 
+    //validate address-street-number
+    if (empty($_POST["streetnumber"])){
+         $errMessage=$errMessage.", Street Number is empty !";
+    }else{
+     $streetNumber=cleanInput($_POST["streetnumber"]); 
+     if (!filter_var($streetNumber,FILTER_VALIDATE_INT) ){
+         $errMessage=$errMessage.", Street number Only number allowed !"; 
+     }
+    }
+ 
+    //validate address-city
+    if (empty($_POST["city"])){
+         $errMessage=$errMessage.", City is empty";
+    }else{
+     $city=cleanInput($_POST["city"]); 
+    }
+ 
+    //validate address-Zip Code
+    if (empty($_POST["zipcode"])){
+         $errMessage=$errMessage.", ZipCode is empty !";
+    }else{
+     $zipCode=cleanInput($_POST["zipcode"]); 
+     if(!filter_var($zipCode,FILTER_VALIDATE_INT)){
+      $errMessage=$errMessage.", Zipcode Only number allowed !"; 
+     }
+    }    
+    
+   //Validate product Selection
+   if (empty($_POST["products"])){
+    $errMessage=$errMessage.", At least one product has to be selected (Order empty) :( !"; 
+   }else{
+     foreach ($_POST["products"] as $index => $productOrdered) {            
+      $selectedProducts[$products[$index]['name']] =$productOrdered;   
+     }
+    
+   }
+
+   if (!empty($errMessage)){
+     $errMessage="Please check and fix this issues :) : ".$errMessage;
+     $errMessage= " <div class='alert alert-dismissible alert-warning'>
+     <button type='button' class='close' data-dismiss='alert'>&times;</button> 
+     <h4 class='alert-heading'>Warning!</h4> 
+     <p class='mb-0'>$errMessage
+     </p>
+ </div>";
+    return false; 
+   } else{
+     return true;
+   }
+ };
+ 
 
 // function whatIsHappening() {
 //     echo '<h2>$_GET</h2>';
@@ -16,20 +133,20 @@ session_start();
 //     var_dump($_SESSION);
 // }
 
-//your products with their price.
-$products = [
-    ['name' => 'Club Ham', 'price' => 3.20],
-    ['name' => 'Club Cheese', 'price' => 3],
-    ['name' => 'Club Cheese & Ham', 'price' => 4],
-    ['name' => 'Club Chicken', 'price' => 4],
-    ['name' => 'Club Salmon', 'price' => 5],
-    ['name' => 'Cola', 'price' => 2],
-    ['name' => 'Fanta', 'price' => 2],
-    ['name' => 'Sprite', 'price' => 2],
-    ['name' => 'Ice-tea', 'price' => 3],
-];
 
+// whatIsHappening(); 
+if ($_SERVER["REQUEST_METHOD"]=="POST"){    
+  // whatIsHappening(); 
+  if(validateData()){    
+      //Saving in session variables
+      $_SESSION["s_addressStreet"]=$addressStreet;
+      $_SESSION["s_streetNumber"]=$streetNumber;
+      $_SESSION["s_city"]=$city;
+      $_SESSION["s_zipCode"]=$zipCode;
+      
+    };
+     var_dump($_POST);
+};
 
-$totalValue = 0;
-
+// whatIsHappening();
 require 'orderForm.php';
--- orderForm.php
+++ orderForm.php
@@ -31,54 +31,87 @@
                 </li>
             </ul>
         </nav>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <?php echo $errMessage ?>
+        <form method="post" action="">
             <div class=" form-row">
                 <div class="form-group col-md-6">
                     <label for="email">E-mail:</label>
-                    <input type="text" id="email" name="email" class="form-control" />
+                    <input type="text" id="email" name="email" class="form-control" value="<?php echo $email;?>" />
                 </div>
                 <div></div>
             </div>
-
             <fieldset>
                 <legend>Address</legend>
-
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="street">Street:</label>
-                        <input type="text" name="street" id="street" class="form-control">
+                        <input type="text" name="street" id="street" class="form-control"
+                            value="<?php echo $addressStreet;?>">
                     </div>
                     <div class="form-group col-md-6">
                         <label for="streetnumber">Street number:</label>
-                        <input type="text" id="streetnumber" name="streetnumber" class="form-control">
+                        <input type="text" id="streetnumber" name="streetnumber" class="form-control"
+                            value="<?php echo $streetNumber;?>">
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="city">City:</label>
-                        <input type="text" id="city" name="city" class="form-control">
+                        <input type="text" id="city" name="city" class="form-control" value="<?php echo $city;?>">
                     </div>
                     <div class="form-group col-md-6">
                         <label for="zipcode">Zipcode</label>
-                        <input type="text" id="zipcode" name="zipcode" class="form-control">
+                        <input type="text" id="zipcode" name="zipcode" class="form-control"
+                            value="<?php echo $zipCode;?>">
                     </div>
                 </div>
             </fieldset>
-
-            <fieldset>
-                <legend>Products</legend>
-                <?php foreach ($products AS $i => $product): ?>
-                <label>
-                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]" /> <?php echo $product['name'] ?>
-                    -
-                    &euro; <?php echo number_format($product['price'], 2) ?></label><br />
-                <?php endforeach; ?>
-            </fieldset>
-
-            <button type="submit" class="btn btn-primary">Order!</button>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <fieldset>
+                        <legend>Products</legend>
+                        <?php foreach ($products AS $i => $product): ?>
+                        <label>
+                            <input type="checkbox" value="1" name="products[<?php echo $i ?>]"
+                                <?php  echo isset($selectedProducts[$product['name']]) ? "checked" : ""; ?> />
+                            <?php echo $product['name'] ?> -
+                            &euro; <?php echo number_format($product['price'], 2) ?></label><br />
+                        <?php endforeach; ?>
+                    </fieldset>
+                </div>
+                <div class="form-group col-md-6">
+                    <fieldset>
+                        <div class="card text-white bg-info mb-3 float-right" style="max-width: 20rem;">
+                            <div class="card-header">Delivery Options</div>
+                            <div class="card-body">
+                                <p class="card-text">
+                                <div class="custom-control custom-radio">
+                                    <input type="radio" id="customRadio1" name="delivery" value="normal"
+                                        class="custom-control-input" checked="">
+                                    <label class="custom-control-label" for="customRadio1">Normal delivery (2
+                                        Hrs.)</label>
+                                </div>
+                                <div class="custom-control custom-radio">
+                                    <input type="radio" id="customRadio2" name="delivery" value="express"
+                                        class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadio2">Express delivery (45
+                                        mins.)</label>
+                                </div>
+                                </p>
+                            </div>
+                    </fieldset>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <button type="submit" class="btn btn-primary">Order!</button>
+                    </div>
+                </div>
         </form>
 
-        <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in food and drinks.</footer>
+        <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in food
+            and
+            drinks.</footer>
+
     </div>
 
     <style>
